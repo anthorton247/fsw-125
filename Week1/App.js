cb34a4abd88be9b85874b4695ee9df4ad10f1272
Anthony Horton <anthorton247@gmail.com>
2020-06-07 19:47:30 -0400

    Start Up

---
+++ Week1/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import List from "./List.js"
+
+function App(){
+      return(
+        <div>
+          <List />
+        </div>
+      )
+}
+
+export default App
---
+++ Week1/List.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+    return (
+        <div>
+            <ol>
+                <li>
+                One
+                </li>
+                <li>
+                Two
+                </li>
+                <li>
+                Three
+                </li>
+            </ol>
+        </div>
+    )
+}
+
+export default App
